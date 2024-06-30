52244e1b64e6630c2c141b5c36d5c63f22f35fff
ivyaeri <80895402+ivyaeri@users.noreply.github.com>
2024-06-30 15:42:46 +0100

    Update new.js

--- new.js
+++ new.js
@@ -2,5 +2,17 @@ let mainNav = document.getElementById('js-menu');
 let navBarToggle = document.getElementById('js-navbar-toggle');
 
 navBarToggle.addEventListener('click', function () {
-  mainNav.classList.toggle('active');
+    mainNav.classList.toggle('active');
 });
+
+function calculateAge(dob) {
+    const diffMs = Date.now() - dob.getTime();
+    const ageDt = new Date(diffMs);
+
+    return Math.abs(ageDt.getUTCFullYear() - 1970);
+}
+
+const dob = new Date('1998-08-19');
+const age = calculateAge(dob);
+document.getElementById('age').textContent = age;
+
